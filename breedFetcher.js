5edb7f4b80703fe27a4b68efdb606fd8688380b9
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-25 19:16:01 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,24 @@
 const request = require("request");
 const args = process.argv.slice(2);
 
-request(
-  "https://api.thecatapi.com/v1/breeds/search?q=" + args[0], //adjust query based on command line arguments
-  (err, response, body) => {
-    if (err) {
-      console.log(err);
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=" + args[0], //adjust query based on command line arguments
+    (err, response, body) => {
+      if (err) {
+        console.log(err);
+      }
+      const data = JSON.parse(body);
+      const anything = data[0];
+      if (anything) {
+        //grab description of cat if cat exists
+        console.log(anything.description);
+      } else{
+        console.log(`Sorry, we couldn't find ${args[0]}`); //cat doesnt exist
+      }
+      
     }
-    const data = JSON.parse(body);
-    const anything = data[0];
-    if (anything) {
-      //grab description of cat if cat exists
-      console.log(anything.description);
-    }
-    console.log(`Sorry, we couldn't find ${args[0]}`); //cat doesnt exist
-  }
-);
+  );
+};
+
+module.exports = {fetchBreedDescription}
---
+++ index.js
@@ -0,0 +1,10 @@
+const { fetchBreedDescription } = require('./breedFetcher')
+const breedName = process.argv[0]
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
